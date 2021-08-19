52cbfc67de65b5a67507176d1228dd4d30a1384f
Gianluca Romeo <glromeo@gmail.com>
2021-08-19 10:16:28 +0100

    Added case for co-located tools

--- src/utils.ts
+++ src/utils.ts
@@ -7,6 +7,10 @@ function requireModule(module: string, includePaths: string[] | undefined) {
     try {
         return require(require.resolve(module, includePaths ? {paths: includePaths} : {paths: [process.cwd()]}));
     } catch (e) {
+        try {
+            return require(module); // extra attempt at finding a co-located tools
+        } catch (ignored) {
+        }
         console.error(`Cannot find module '${module}', make sure it's installed. e.g. yarn add -D ${module}`, e);
         process.exit(1);
     }
